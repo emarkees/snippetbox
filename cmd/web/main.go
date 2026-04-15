d65fc7ba84dbe40db9f6b069d521bf4db0f47ccc
aolusola <olusolaadeyemi8@gmail.com>
2026-04-15 16:30:55 +0100

    Add more Handlers functions

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -9,12 +9,21 @@ func home(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte( "Hello Yemi!"))
 }
 
+func viewSnippet(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("I can view here"))
+}
+
+func createSnippet(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Create a new Snippet"))
+}
+
 
 func main() {
 	mux := http.NewServeMux()
 
 	// handlers
 	mux.HandleFunc("/", home)
+	mux.HandleFunc("/view", viewSnippet)
 
 	log.Println("Server is running on Port:8000")
 	err := http.ListenAndServe(":8000", mux)
