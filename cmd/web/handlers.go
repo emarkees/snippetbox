3dcca394260c2b22847adcb36f6c8a41e292f8a6
aolusola <olusolaadeyemi8@gmail.com>
2026-04-17 16:26:15 +0100

    Add view functon

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -8,3 +8,13 @@ import (
 func home(c *gin.Context){
 	c.String(http.StatusOK, "We are home")
 }
+
+func viewSnippet(c *gin.Context) {
+	idx := Query("id")
+
+	err, id := strconv.Atoi(idx)
+	if err != nil || id < 1 {
+		c.String(http.StatusNotFound, "Snippet not found")
+		return
+	}
+}
