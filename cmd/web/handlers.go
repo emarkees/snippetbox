d8dee6055574263944d862ab4dc9a2da438fc779
Emarkees <emintechsolutions@gmail.com>
2026-04-22 01:57:11 +0100

    Feat; Update main.go file, handlers files

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -2,19 +2,49 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
+	"log"
 	"net/http"
+	"strconv"
+	"html/template"
 )
 
-func home(c *gin.Context){
-	c.String(http.StatusOK, "We are home")
+func (app *application) home(c *gin.Context) {
+	files := []string{
+		"./ui/html/base.tmpl",
+		"./ui/html/pages/home.tmpl",
+		"./ui/html/partials/nav.tmpl",
+	}
+
+	ts, err := template.ParseFiles(files...)
+	if err != nil {
+		app.ErrorLog.Println(err.Error())
+		c.String(http.StatusInternalServerError, "Internal Server Error")
+		return
+	}
+
+	err = ts.ExecuteTemplate(c.Writer, "base", nil)
+	if err != nil {
+		app.errorLog.Println(err.Error())
+		c.String(http.StatusInternalServerError, "Internal Server Error")
+	}
 }
 
-func viewSnippet(c *gin.Context) {
-	idx := Query("id")
-
-	err, id := strconv.Atoi(idx)
+func (app *application)viewSnippet(c *gin.Context) {
+	idx := c.Query("id")
+	id, err := strconv.Atoi(idx)
 	if err != nil || id < 1 {
 		c.String(http.StatusNotFound, "Snippet not found")
 		return
 	}
+	c.String(http.StatusOK, "Snippet ID: %d", id)
+}
+
+func (app *application) createSnippet(c *gin.Context) {
+	if c.Request.Method != http.MethodPost {
+		c.Header("Allow", http.MethodPost)
+		c.String(http.StatusMethodNotAllowed, "Method not Allowed")
+		return
+	}
+
+	c.String(http.StatusOK, "Snippet created successfully")
 }
--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,27 +1,73 @@
 package main
 
 import (
+	"flag"
 	"github.com/gin-gonic/gin"
 	"log"
 	"net/http"
-)
+	"os"
+	
+	// Define an application struct to hold the application-wide dependencies for the
+	// web application. For now we'll only include fields for the two custom loggers, but
+	// we'll add more to it as the build progresses.
+
+	type application struct{
+		errorLog *log.Logger,
+		infoLog *log.Logger
+	}
 
 func main () {
-	r := gin.Default()
 
-	// no Route
-	r.NoRoute(func(c *gin.Context){
-		c.String(http.StatusNotFound, "404 page not found")
+	// a new command line flag is defined
+	addr := flag.String("addr", ":4000", "HTTP network address")
+
+	flag.Parse()
+
+	// Creating a new error log
+	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+
+	// Create a logger for written error message
+	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+
+	// Instead of using Default we use NEW
+	r := gin.New()
+	r.Use(gin.Recovery())
+	r.LoadHTMLGlob("./ui/html/pages/*")
+	
+	// This part serve the static files also remove the prefix
+	r.StaticFS("/static", http.Dir("./ui/static"))
+
+	// Custom 404 for unmatched routes
+	r.NoRoute(func(c *gin.Context) {
+		c.HTML(http.StatusNotFound, "404.tmpl", gin.H{
+			"message": "Page not found",
+		})
+		// or: c.String(http.StatusNotFound, "Not Found")
 	})
 
 	r.NoMethod(func(c *gin.Context) {
-		c.String(http.StatusMethodNotFound, "405 Method not Allowed")
+		c.Header("Allow", "GET, POST")
+		c.String(http.StatusMethodNotAllowed, "Method Not Allowed")
 	})
 
-	// add routers
-	r.GET("/", home)
+	app := &application{
+		errorLog: errorLog
+		infoLog: infoLog
+	}
 
-	log.Println("Server is running on Port: 4000")
-	err := r.Run()
-	log.Fatal(err)
+	// add routers
+	r.GET("/", app.home)
+	r.GET("/snippet/view", app.viewSnippet)
+	r.Any("/snippet/create", app.createSnippet)
+
+	// Initializing a new server Struct
+	srv := &http.Server{
+		Addr: *addr,
+		ErrorLog: errorLog,
+		Handler: r
+	}
+
+	infoLog.Printf("Server is running on Port %s:", *addr)
+	err := srv.Run()
+	errorLog.Fatal(err)
 }
