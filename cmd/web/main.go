6a4d0a3b45f7888ec9904b39dd1c05c975d2ae0d
aolusola <olusolaadeyemi8@gmail.com>
2026-04-22 13:59:13 +0100

    feat:creating a database connection pool

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"database/sql"
 	"flag"
 	"log"
 	"net/http"
 	"os"
- 	)
+
+	_ "github.com/go-sql-driver/mysql"
+)
 	
 	// Define an application struct to hold the application-wide dependencies for the
 	// web application. For now we'll only include fields for the two custom loggers, but
@@ -21,6 +24,8 @@ func main () {
 	// a new command line flag is defined
 	addr := flag.String("addr", ":4000", "HTTP network address")
 
+	dsn := flag.String("dsn", "web:pass@/snippetbox?parseTime=true", "MySQL data source name")
+
 	flag.Parse()
 
 	// Creating a new error log
@@ -28,6 +33,14 @@ func main () {
 
 	// Create a logger for written error message
 	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+
+	db, err := openDB(*dsn)
+	if err != nil {
+		errorLog.Fatal(err)
+	}
+
+	defer db.Close()
+
 
 	app := &application{
 		errorLog: errorLog,
@@ -44,6 +57,18 @@ func main () {
 	}
 
 	infoLog.Printf("Server is running on Port %s:", *addr)
-	err := srv.ListenAndServe()
+	err = srv.ListenAndServe()
 	errorLog.Fatal(err)
+}
+
+func openDB(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	if err = db.Ping(); err != nil {
+		return nil, err
+	}
+	return db, nil
 }
